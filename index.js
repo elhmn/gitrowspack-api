badd3bf917f634a6369bb83ec95f80d0bb1f8380
elhmn <nleme@live.fr>
2022-01-09 20:49:32 +0100

    feat: reformat code

--- index.js
+++ index.js
@@ -2,124 +2,118 @@
 // gitrows support .json|.csv|.yaml
 
 // We set the dotenv
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 // We instantiate our gitrows
-const Gitrows = require('./gitrows');
+const Gitrows = require("./gitrows");
 // We call express
-const express = require('express')
+const express = require("express");
 
 dotenv.config();
-const app = express()
-const port = `${process.env.PORT}`
-const version = `${process.env.VERSION}`
-const EXTENSION = `json` // yaml, csv, json
-
+const app = express();
+const port = `${process.env.PORT}`;
+const version = `${process.env.VERSION}`;
+const EXTENSION = `json`; // yaml, csv, json
 
 const options = {
   token: `${process.env.GITHUB_TOKEN}`,
   owner: `${process.env.GITHUB_OWNER}`,
-  csv: { delimiter: ',' },
+  csv: { delimiter: "," },
   cacheTTL: 7000, // You can tweak this value to have a custom cache validity time (3s here)
   type: EXTENSION,
-}
+};
 
-const getPath = (req, ns='github') => {
-    const formatExtension = req.query.format ? req.query.format : EXTENSION;
-    return `@${ns}/${process.env.GITHUB_OWNER}/${req.params.project}/${req.params.database}/${req.params.collection}.${formatExtension}`
-}
+const getPath = (req, ns = "github") => {
+  const formatExtension = req.query.format ? req.query.format : EXTENSION;
+  return `@${ns}/${process.env.GITHUB_OWNER}/${req.params.project}/${req.params.database}/${req.params.collection}.${formatExtension}`;
+};
 
 const gitrows = new Gitrows(options);
-const BASE_ROUTE = '/api/v1';
+const BASE_ROUTE = "/api/v1";
 
 app.use(express.json());
-
 
 // get with filters
 // get all elements
 // curl -X POST -H 'content-type: application/json' http://127.0.0.1:3030/api/v1/data/d
 // curl -X POST -H 'content-type: application/json' -d '{"filter": {"key": "1"}}' http://127.0.0.1:3030/api/v1/get/data/d/o
-app.post(`${BASE_ROUTE}/get/:project/:database/:collection`, async (req, res) => {
+app.post(
+  `${BASE_ROUTE}/get/:project/:database/:collection`,
+  async (req, res) => {
     const filter = req.body["filter"];
 
-    const data = await gitrows.get(
-        getPath(req),
-        filter
-    );
-    res.json(data)
-})
-
+    const data = await gitrows.get(getPath(req), filter);
+    res.json(data);
+  }
+);
 
 // Now we can put elements
 // curl -X POST -H 'content-type: application/json' -d '{"data": {"key": "0001"}}' http://127.0.0.1:3030/api/v1/put/data/d/o
-app.post(`${BASE_ROUTE}/put/:project/:database/:collection`, async (req, res) => {
+app.post(
+  `${BASE_ROUTE}/put/:project/:database/:collection`,
+  async (req, res) => {
     const dataToInsert = req.body["data"];
 
-    const data = await gitrows.put(
-        getPath(req),
-        dataToInsert
-    );
-    res.json(data)
-})
+    const data = await gitrows.put(getPath(req), dataToInsert);
+    res.json(data);
+  }
+);
 
 // Now we can update existing elements
 // curl -X POST -H 'content-type: application/json' -d '{"data": {"value": "new"}, "filter": {"key": "ooo1"}}}' /api/v1/update/data/d/o
-app.post(`${BASE_ROUTE}/update/:project/:database/:collection`, async (req, res) => {
+app.post(
+  `${BASE_ROUTE}/update/:project/:database/:collection`,
+  async (req, res) => {
     const filter = req.body["filter"];
     const dataToInsert = req.body["data"];
 
-    const data = await gitrows.update(
-        getPath(req),
-        dataToInsert,
-        filter
-    );
-    res.json(data)
-})
+    const data = await gitrows.update(getPath(req), dataToInsert, filter);
+    res.json(data);
+  }
+);
 
 // Now we can replace the whole collection
 // curl -X POST -H 'content-type: application/json' -d '{"data": [ {"key": "o9o1",  "value": "new"} ]' /api/v1/replace/data/d/o
-app.post(`${BASE_ROUTE}/replace/:project/:database/:collection`, async (req, res) => {
+app.post(
+  `${BASE_ROUTE}/replace/:project/:database/:collection`,
+  async (req, res) => {
     const dataToInsert = req.body["data"];
 
-    const data = await gitrows.replace(
-        getPath(req),
-        dataToInsert,
-    );
-    res.json(data)
-})
-
+    const data = await gitrows.replace(getPath(req), dataToInsert);
+    res.json(data);
+  }
+);
 
 // Now we can delete existing elements
 // curl -X POST -H 'content-type: application/json' -d '{"filter": {"key": "ooo1"}}}' /api/v1/delete/data/d/o
-app.post(`${BASE_ROUTE}/delete/:project/:database/:collection`, async (req, res) => {
+app.post(
+  `${BASE_ROUTE}/delete/:project/:database/:collection`,
+  async (req, res) => {
     const filter = req.body["filter"];
 
-    const data = await gitrows.delete(
-        getPath(req),
-        filter
-    );
-    res.json(data)
-})
+    const data = await gitrows.delete(getPath(req), filter);
+    res.json(data);
+  }
+);
 
 // Now we can create a new collection
 // curl -X POST -H 'content-type: application/json' /api/v1/create/data/d2/o
-app.post(`${BASE_ROUTE}/create/:project/:database/:collection`, async (req, res) => {
-
-    const data = await gitrows.create(
-        getPath(req)
-    );
-    res.json(data)
-})
+app.post(
+  `${BASE_ROUTE}/create/:project/:database/:collection`,
+  async (req, res) => {
+    const data = await gitrows.create(getPath(req));
+    res.json(data);
+  }
+);
 
 // Now we can drop a whole collection
 // curl -X POST -H 'content-type: application/json' /api/v1/drop/data/d2/o
-app.post(`${BASE_ROUTE}/drop/:project/:database/:collection`, async (req, res) => {
-
-    const data = await gitrows.drop(
-        getPath(req)
-    );
-    res.json(data)
-})
-
+app.post(
+  `${BASE_ROUTE}/drop/:project/:database/:collection`,
+  async (req, res) => {
+    const data = await gitrows.drop(getPath(req));
+    res.json(data);
+  }
+);
 
 // Some cli endpoints for checking
 // -------------------------------
@@ -127,38 +121,40 @@ app.post(`${BASE_ROUTE}/drop/:project/:database/:collection`, async (req, res) =
 // A simple ping for checking if the service is up
 // curl /api/v1/ping
 app.get(`${BASE_ROUTE}/ping`, async (req, res) => {
-    res.json({
-        "status": "success",
-        "server-ip": req.hostname,
-        "version": `GitRowsPack-Api-${version}`,
-        "message": `Welcome to the GitRowsPack shell.
+  res.json({
+    status: "success",
+    "server-ip": req.hostname,
+    version: `GitRowsPack-Api-${version}`,
+    message: `Welcome to the GitRowsPack shell.
 For interactive help, type "help".
 For more comprehensive documentation, see
-	https://github.com/Sanix-Darker/gitrowspack-api`
-    });
+	https://github.com/Sanix-Darker/gitrowspack-api`,
+  });
 });
 
 // A simple endpoint to get the list of databases
 // curl /api/v1/databases
 app.get(`${BASE_ROUTE}/:project/databases`, async (req, res) => {
-    res.json({
-        "project": req.params.project,
-        "databases": await gitrows.getDatabases(req.params.project)
-    });
+  res.json({
+    project: req.params.project,
+    databases: await gitrows.getDatabases(req.params.project),
+  });
 });
 
 // A simple endpoint to get the list of databases
 // curl /api/v1/collections
 app.get(`${BASE_ROUTE}/:project/:database/collections`, async (req, res) => {
-    res.json({
-        "project": req.params.project,
-        "database": req.params.database,
-        "collections": await gitrows.getCollections(req.params.project, req.params.database)
-    });
+  res.json({
+    project: req.params.project,
+    database: req.params.database,
+    collections: await gitrows.getCollections(
+      req.params.project,
+      req.params.database
+    ),
+  });
 });
 
-
 app.listen(port, () => {
-    console.log('-------------------------------------------------------')
-    console.log(`GitRowsPack-Api-${version} started at http://localhost:${port}`)
-})
+  console.log("-------------------------------------------------------");
+  console.log(`GitRowsPack-Api-${version} started at http://localhost:${port}`);
+});
